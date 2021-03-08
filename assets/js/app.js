504415f304c36381e4a41352ab0f607d89bc12b0
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-03-07 21:47:35 -0800

    work on serach and bookmark section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,59 @@
+var cityNameEl = document.querySelector("#city-name");
+var searchInputEl = document.querySelector(".search-city");
+var searchButtonEl = document.querySelector("#search-button");
+var bookmarksEl = document.querySelector("#bookmark");
+var searchBookmark = JSON.parse(localStorage.getItem("search")) || [];
+
 const apiKey = "HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&";
+
+function getWeather(city) {
+
+    let weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+    // Dates and Days section
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+    
+
+    fetch(weatherAPI).then(function(response) {
+
+        // weather card section
+        cityNameEl.innerHTML = response.data.name + "(" + month + "-" + day + "-" + year;
+        // temperature 
+        var temp = parseInt((response.main.temp)* 9/5 - 459);
+        var cityTemp = $("<p>").text("Temp: " + temp + " Fahrenheit");
+        $("#temp").append(cityTemp);
+        // humidity 
+        var cityHumid = $("<p>").text("Humdity: " + response.main.humidty + " %");
+        $("#humid").append(cityHumid);
+        // Wind Speeds
+        var windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " mph");
+        $("#wind-speed").append(windSpeed);
+    });
+};
+
+// Search and Bookmark section
+function showSearchBookmarks() {
+    bookmarksEl.innerHTML = "";
+    for (var i = 0; i < searchBookmark.length; i++) {
+        var bookmarkInput = document.createElement("input");
+        bookmarkInput.setAttribute("type", "text");
+        bookmarkInput.setAttribute("class", "form-control d-block bg-white");
+        bookmarkInput.setAttribute("value", searchBookmark[i]);
+        bookmarkInput.addEventListener("click", function() {
+            getWeather(bookmarkInput.value);
+        })
+        bookmarksEl.append(bookmarkInput);
+    }
+}
+
+// button section for search button
+searchButtonEl.addEventListener("click", function() {
+    var searchCity = searchInputEl.value;
+    getWeather(searchCity);
+    searchBookmark.push(searchCity);
+    localStorage.setItem("search", JSON.stringify(searchBookmark));
+    showSearchBookmarks();
+})
